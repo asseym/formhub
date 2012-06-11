04e4cb799174e23a57710ca2257c425f2d866ec9
Larry Weya <larryweya@gmail.com>
2012-06-11 14:32:12 +0300

    Removed build_workbook_for function since that functionality is implemented by WorkBook class

--- odk_viewer/pandas_mongo_bridge.py
+++ odk_viewer/pandas_mongo_bridge.py
@@ -8,23 +8,6 @@ from utils.export_tools import question_types_to_exclude
 
 xform_instances = settings.MONGO_DB.instances
 
-def build_workbook_for(username, id_string):
-    """
-    Creates a workbook composed of multiple sheets
-    """
-    w = WorkBook()
-    dd = DataDictionary.objects.get(user__username=username, id_string=id_string)
-
-
-
-
-    query = {ParsedInstance.USERFORM_ID: u'%s_%s' % (username, id_string)}
-    #TODO: how to query 50 thousands records
-    cursor = xform_instances.find(query)
-    instances = list(instance for instance in cursor)
-    #TODO: if instances is empty the DataFrame constructor throws
-    return DataFrame(instances)
-
 class Sheet(object):
     """
     Represents a single row/column data structure essentially a single DataFrame object
